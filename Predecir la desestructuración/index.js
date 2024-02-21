be27a41ca67911c6e40b392e0f4ea679e862e29c
FabianCiceia <ciceiafabian@gmail.com>
2024-02-21 12:09:07 -0300

    generando archivo

---
+++ Predecir la desestructuración/index.js
@@ -0,0 +1,28 @@
+// Problema  1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+/* 
+Salida:
+Tesla
+Mercedes
+*/
+
+// Problema 2 
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+/*
+Salida:
+name not define (La variable name no esta definida)
+Elon
+*/
