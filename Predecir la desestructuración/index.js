982fd2010c002a8bc74bddd5c995e52a84e6fd11
FabianCiceia <ciceiafabian@gmail.com>
2024-02-21 12:38:33 -0300

    resolviendo los ejercicios de Predecir la desestructuración

--- Predecir la desestructuración/index.js
+++ Predecir la desestructuración/index.js
@@ -26,3 +26,57 @@ Salida:
 name not define (La variable name no esta definida)
 Elon
 */
+
+//problema 3 
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+/*
+Salida:
+12345
+Indefinido (ya que no existe ninguna variable llamado password en el objecto person)
+*/
+
+//Problema 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+/*
+Salida:
+false
+true
+*/
+
+//problema 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+/*
+Salida:
+value
+[1,5,1,8,3,3]
+1
+5
+*/
+
